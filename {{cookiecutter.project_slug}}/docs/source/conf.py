70c37d929b53e80be63fb48706d2526c63bfa7b7
kuter <kuter@ktr.x25.pl>
2017-03-02 22:00:19 +0100

    documentation

--- {{cookiecutter.project_slug}}/docs/source/conf.py
+++ {{cookiecutter.project_slug}}/docs/source/conf.py
@@ -38,6 +38,7 @@ get_wsgi_application()
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinxcontrib.programoutput',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -58,7 +59,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = '{{cookiecutter.project_slug|title}}'
-copyright = '2016, {{cookiecutter.full_name}}'
+copyright = '2017, {{cookiecutter.full_name}}'
 author = '{{cookiecutter.full_name}}'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -345,3 +346,53 @@ texinfo_documents = [
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #
 # texinfo_no_detailmenu = False
+
+
+# Example configuration for intersphinx: refer to the Python standard library.
+intersphinx_mapping = {'https://docs.python.org/': None}
+
+THIS_DIR = os.path.dirname(__file__)
+PROJECT_DIR = os.path.join(THIS_DIR, '../')
+sys.path.append(PROJECT_DIR)
+
+import inspect  # noqa
+from django.utils.html import strip_tags  # noqa
+from django.utils.encoding import force_text  # noqa
+
+
+def process_docstring(app, what, name, obj, options, lines):
+    """" Causes import errors if left outside the function. """
+    from django.db import models
+
+    # Only look at objects that inherit from Django's base model class
+    if inspect.isclass(obj) and issubclass(obj, models.Model):
+        # Grab the field list from the meta class
+        fields = obj._meta.fields
+
+        for field in fields:
+            # Decode and strip any html out of the field's help text
+            help_text = strip_tags(force_text(field.help_text))
+
+            # Decode and capitalize the verbose name, for use if there isn't
+            # any help text
+            verbose_name = force_text(field.verbose_name).capitalize()
+
+            if help_text:
+                # Add the model field to the end of the docstring as a param
+                # using the help text as the description
+                lines.append(u':param %s: %s' % (field.attname, help_text))
+            else:
+                # Add the model field to the end of the docstring as a param
+                # using the verbose name as the description
+                lines.append(u':param %s: %s' % (field.attname, verbose_name))
+
+            # Add the field's type to the docstring
+            lines.append(u':type %s: %s' % (field.attname, type(field).__name__))
+
+    # Return the extended docstring
+    return lines
+
+
+def setup(app):
+    """ Register the docstring processor with sphinx. """
+    app.connect('autodoc-process-docstring', process_docstring)
--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
@@ -2,7 +2,63 @@
 Installation
 ============
 
-Configuration
-=============
+Getting the code
+----------------
+
+.. code:: bash
+
+    $ pip install {{cookiecutter.project_slug}}
+
+Prerequisites
+-------------
+
+Make sure that ``'django.contrib.staticfiles'`` is `set up properly
+<https://docs.djangoproject.com/en/stable/howto/static-files/>`_ and add
+``'{{cookiecutter.project_slug}}'`` to your ``INSTALLED_APPS`` setting::
+
+    INSTALLED_APPS = [
+        # ...
+        'django.contrib.staticfiles',
+        # ...
+        '{{cookiecutter.project_slug}}',
+    ]
+
+    STATIC_URL = '/static/'
+
+URLconf
+-------
+
+Add the {{cookiecutter.project_name}}'s URLs to your project's URLconf as follows::
+
+    from django.conf import settings
+    from django.conf.urls import include, url
+
+    urlpatterns = [
+        url(r'^{{cookiecutter.project_slug}}/', include('{{cookiecutter.project_slug}}.urls')),
+    ]
+
+Application
+===========
+
+Models
+------
+
+.. automodule:: {{cookiecutter.project_slug}}.models
+    :members:
+    :show-inheritance:
+
+Views
+-----
+
+.. automodule:: {{cookiecutter.project_slug}}.views
+    :members:
+    :show-inheritance:
+
+Templates
+---------
+
+.. program-output:: tree ../../{{cookiecutter.project_slug}}/templates/
+
+
 """
 default_app_config = '{{cookiecutter.project_slug}}.apps.{{cookiecutter.project_slug|title}}Config'

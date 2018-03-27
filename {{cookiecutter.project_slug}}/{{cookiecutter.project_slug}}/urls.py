99ff1247854cf9dcd0588c5dafa9b93d2619109f
kuter <contact@devktr.pl>
2018-03-27 20:49:24 +0200

    urls.py fix

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/urls.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.{{cookiecutter.model}}ListView.as_view(), name='{{cookiecutter.model}}-list'),
-    path('<int:pk>/', views.{{cookiecutter.model}}DetailView.as_view(), name='{{cookiecutter.model}}-detail')
+    path('', views.{{cookiecutter.model}}ListView.as_view(), name='{{cookiecutter.model.lower()}}-list'),
+    path('<int:pk>/', views.{{cookiecutter.model}}DetailView.as_view(), name='{{cookiecutter.model.lower()}}-detail')
 ]

bdc698c02fe2322682b054e09f1d973784a57a3c
kuter <contact@devktr.pl>
2018-03-27 20:32:19 +0200

    unicode docstring removed

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/tests/test_models.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/tests/test_models.py
@@ -1,16 +1,15 @@
-# -*- coding: utf-8 -*-
 from django.test import TestCase
 from ..models import {{cookiecutter.model}}
 
 
 class {{cookiecutter.model}}Tests(TestCase):
 
-    u"""Test class for {{cookiecutter.model}}."""
+    """Test class for {{cookiecutter.model}}."""
 
     def setUp(self):
-        u"""Setup for {{cookiecutter.model}}."""
+        """Setup for {{cookiecutter.model}}."""
         self.obj = {{cookiecutter.model}}()
 
     def test_isinstance(self):
-        u"""Test for isinstance method."""
+        """Test for isinstance method."""
         self.assertTrue(isinstance(self.obj, {{cookiecutter.model}}))

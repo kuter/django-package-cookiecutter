c256ba333b235b34ad0aa9a441d315dd30906a5e
kuter <contact@devktr.pl>
2019-11-07 21:09:47 +0100

    fixed docker-entrypoint

--- {{cookiecutter.project_slug}}/docker-entrypoint.sh
+++ {{cookiecutter.project_slug}}/docker-entrypoint.sh
@@ -2,4 +2,4 @@
 python manage.py migrate
 python manage.py collectstatic --noinput
 pip install gunicorn
-gunicorn --workers 2 --bind 0.0.0.0:8000 --timeout=600 pay2win.wsgi:application
+gunicorn --workers 2 --bind 0.0.0.0:8000 --timeout=600 example.wsgi:application
